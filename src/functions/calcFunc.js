12571c0b5ab2ab43c5e4ad9179ecd44a76d6978c
Angel Javier <angel_java@hotmail.com>
2025-06-25 01:31:24 +0000

    calcFunc was added to project

---
+++ src/functions/calcFunc.js
@@ -0,0 +1,25 @@
+const { app } = require('@azure/functions');
+
+app.http('calcFunc', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const a = parseFloat(request.query.get('a'));
+        const b = parseFloat(request.query.get('b'));
+
+        if (isNaN(a) || isNaN(b)) {
+            return {
+                status: 400,
+                body: 'Los parámetros "a" y "b" deben ser números válidos.'
+            };
+        }
+
+        const c = a + b;
+        return {
+            body: `${a} + ${b} = ${c}`
+        };
+    }
+});
+
